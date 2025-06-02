94ac34f35a503bef3932d927e411488940145437
Julia <45031945+JuliaNowakowska@users.noreply.github.com>
2025-06-02 16:41:13 +0100

    feat: new lyrics table and insert

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 import sqlite3
 from config import DB_FILE
 from track import Track
+from lyrics import Lyrics
 
 class Database:
     def __init__(self):
@@ -9,6 +10,7 @@ class Database:
 
     def _create_table(self):
         with self.conn:
+            # Spotify table
             self.conn.execute('''
                 CREATE TABLE IF NOT EXISTS favourite_tracks (
                     id TEXT PRIMARY KEY,
@@ -18,12 +20,30 @@ class Database:
                 )
             ''')
 
+            # Genius table
+            self.conn.execute('''
+                CREATE TABLE IF NOT EXISTS lyrics(
+                    genius_id TEXT PRIMARY KEY,
+                    spotify_id TEXT NOT NULL,
+                    lyrics TEXT NOT NULL,
+                    FOREIGN KEY (spotify_id) REFERENCES favourite_tracks(id)
+                )
+            ''')
+
+
     def insert_track(self, track: Track):
         with self.conn:
             self.conn.execute('''
                 INSERT OR REPLACE INTO favourite_tracks (id, name, artists, url)
                 VALUES (?, ?, ?, ?)
             ''', (track.id, track.name, track.artists, track.url))
+
+    def insert_lyrics(self, lyrics: Lyrics):
+        with self.conn:
+            self.conn.execute('''
+                INSERT OR REPLACE INTO lyrics (genius_id, spotify_id, lyrics)
+                VALUES (?, ?, ?)
+            ''', (lyrics.genius_id, lyrics.spotify_id, lyrics.lyrics))
 
     def get_all_tracks(self):
         cursor = self.conn.cursor()
