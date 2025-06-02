324637bfe41bb8ccdc001f9838b125a915eb77a8
Julia <45031945+JuliaNowakowska@users.noreply.github.com>
2025-06-02 16:48:38 +0100

    feat: inserting lyrics to the db

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from spotify_client import SpotifyClient
 from genius_client import GeniusClient
 from db import Database
+from lyrics import Lyrics
 
 def main():
     db = Database()
@@ -15,11 +16,14 @@ def main():
 
     # identifying the songs' Genius ID and getting the lyrics
     artist_track = db.get_track_artist()
-    for track, artist in artist_track:
-        song_id = genius.get_song_id(track, artist)
-        if song_id:
-            print(genius.client.lyrics(song_id=song_id, remove_section_headers=True))
+    for spotify_id, track, artist in artist_track:
+        genius_id = genius.get_song_id(track, artist)
 
+        # If the song is found on Genius, insert it to our database
+        if genius_id:
+            lyrics_text = genius.client.lyrics(song_id=genius_id, remove_section_headers=True)
+            lyrics = Lyrics(genius_id, spotify_id, lyrics_text)
+            db.insert_lyrics(lyrics)
 
 
 if __name__ == "__main__":
