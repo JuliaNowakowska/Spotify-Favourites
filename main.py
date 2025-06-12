e1e5eeee8b79d79c3d695b76a537c7002f05a6e9
Julia <45031945+JuliaNowakowska@users.noreply.github.com>
2025-06-12 20:59:44 +0100

    feat: moved classification outside of main

--- main.py
+++ main.py
@@ -16,11 +16,17 @@ def fetch_lyrics(db, genius_client):
     for spotify_id, track, artist in artist_track:
         genius_id = genius_client.get_song_id(track, artist)
 
-        # If the song is found on Genius, insert it to our database
+        # If the song is found on Genius, insert its lyrics to the database
         if genius_id:
             lyrics_text = genius_client.client.lyrics(song_id=genius_id, remove_section_headers=True)
             lyrics = Lyrics(genius_id, spotify_id, lyrics_text)
             db.insert_lyrics(lyrics)
+
+def classify_songs(db, emotion_classifier):
+    classifications = emotion_classifier.classify_emotions(db)
+
+    for id, predicted_label, confidence in classifications:
+        db.insert_classification(id, predicted_label, confidence)
 
 
 def main():
@@ -46,11 +52,10 @@ def main():
 
     if args.classify_emotions:
         print("Classifying emotions in songs")
-        classifications = emotion_classifier.classify_emotions(db)
-        print(classifications)
+        classify_songs(db, emotion_classifier)
 
     if not args.fetch_spotify and not args.fetch_lyrics and not args.classify_emotions:
-        print("No action specified. Use --fetch-spotify and/or --fetch-lyrics.")
+        print("No action specified. Use --fetch-spotify, -fetch-lyrics or --classify-emotions.")
 
 
 if __name__ == "__main__":
